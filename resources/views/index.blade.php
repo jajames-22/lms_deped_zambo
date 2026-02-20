0c07a3bf3fb080534c8419cd81d07b57fbcba3c8
jajames-22 <rojasjamesbenedict@gmail.com>
2026-02-20 16:23:02 +0800

    index enhanced

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -14,7 +14,7 @@
     
 </head>
 <body>
-    <header class="bg-red-700 p-1 flex justify-center items-center">
+    <header style="background-color: #a52a2a;" class="sticky top-0 p-1 flex justify-center z-40 w-full items-center">
         <img src="{{ asset('storage/images/deped_zambo_header.png') }}" 
          class="w-full max-w-5xl h-auto block" 
          alt="DepEd Zamboanga Header">
@@ -22,15 +22,26 @@
     <main>
         <section 
             style="background-image: url('{{ asset('storage/images/deped_zamdiv.jpg') }}');" 
-            class="relative bg-cover bg-center bg-no-repeat pt-32 pb-20 px-6"
+            class="relative bg-cover bg-center bg-no-repeat pt-20 pb-20 px-6"
         >
-            <div class="absolute inset-0 bg-red-900 bg-opacity-10"></div>
-
-            <div class="relative z-10 max-w-7xl mx-auto text-center">
-                <h1 class="text-5xl md:text-7xl font-extrabold text-white mb-6 tracking-tight drop-shadow-2xl">
+            <div class="absolute inset-0 bg-red-900/50"></div>
+            
+            <div class="relative z-10 max-w-7xl mx-auto text-center flex items-center justify-center flex-col">
+                <img src="{{ asset('storage/images/hero-text.png') }}" 
+                class="w-full max-w-xl h-auto block" 
+                alt="DepEd Zamboanga Header">
+                <h1 class="text-5xl md:text-7xl font-extrabold text-white mb-6 tracking-tight text-shadow-lg/30">
                     Learning Management <br> System 
                 </h1>
+                <p class="text-white max-w-2xl">A centralized digital repository and learning management system (LMS) that facilitates the distribution of accessible, division-standard learning materials to ensure educational continuity across Zamboanga City.</p>
+                <div class="block mt-7 g-4">
+                    <button style="background-color: #a52a2a;" class="px-8 py-3 hover:bg-red-800 text-white font-semibold rounded-lg transition-all duration-300 shadow-md hover:shadow-lg transform hover:-translate-y-0.5">Login</button>
+                    <button class="px-8 py-3 bg-white hover:bg-white-100 text-red-700 font-semibold rounded-lg transition-all duration-300 shadow-md hover:shadow-lg transform hover:-translate-y-0.5">Sign Up</button>
+                </div>
             </div>
+        </section>
+        <section class="h-screen">
+
         </section>
     </main>
 </body>
