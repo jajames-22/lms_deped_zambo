f44fca72684b6ca01ffe6ed2a785cf76f3ec6db2
jajames-22 <rojasjamesbenedict@gmail.com>
2026-02-24 13:35:36 +0800

    dashboard

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -14,15 +14,15 @@
     
 </head>
 <body>
-    <header style="background-color: #a52a2a;" class="sticky top-0 p-1 flex justify-center z-40 w-full items-center">
+    <header style="background-color: #a52a2a;" class="fixed p-1 flex justify-center z-40 w-full items-center">
         <img src="{{ asset('storage/images/deped_zambo_header.png') }}" 
-         class="w-full max-w-5xl h-auto block" 
+         class="w-full max-w-4xl h-auto block" 
          alt="DepEd Zamboanga Header">
     </header>
     <main>
         <section 
             style="background-image: url('{{ asset('storage/images/deped_zamdiv.jpg') }}');" 
-            class="relative bg-cover bg-center bg-no-repeat pt-20 pb-20 px-6"
+            class="relative bg-cover bg-center bg-no-repeat pt-50 pb-20 px-6 h-screen"
         >
             <div class="absolute inset-0 bg-red-900/50"></div>
             
@@ -34,9 +34,13 @@
                     Learning Management <br> System 
                 </h1>
                 <p class="text-white max-w-2xl">A centralized digital repository and learning management system (LMS) that facilitates the distribution of accessible, division-standard learning materials to ensure educational continuity across Zamboanga City.</p>
-                <div class="block mt-7 g-4">
-                    <button style="background-color: #a52a2a;" class="px-8 py-3 hover:bg-red-800 text-white font-semibold rounded-lg transition-all duration-300 shadow-md hover:shadow-lg transform hover:-translate-y-0.5">Login</button>
-                    <button class="px-8 py-3 bg-white hover:bg-white-100 text-red-700 font-semibold rounded-lg transition-all duration-300 shadow-md hover:shadow-lg transform hover:-translate-y-0.5">Sign Up</button>
+                <div class="mt-7 flex flex-col w-full sm:flex-row gap-3 justify-center">
+                    <a href="{{ route('dashboard') }}"
+                        style="background-color: #a52a2a;"
+                        class="px-8 py-3 hover:bg-red-800 text-white font-semibold rounded-lg transition-all duration-300 shadow-md hover:shadow-lg transform hover:-translate-y-0.5">
+                        Login
+                    </a>
+                    <button class="px-8 py-3 bg-white hover:bg-white-100 text-red-700 font-semibold rounded-lg transition-all duration-300 shadow-md hover:shadow-lg transform hover:-translate-y-0.5">Create Account</button>
                 </div>
             </div>
         </section>
