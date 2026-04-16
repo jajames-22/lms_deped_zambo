4d1e6eaf6ae223ba26da357c2efe6329f1b9913c
jajames-22 <rojasjamesbenedict@gmail.com>
2026-04-16 09:02:18 +0800

    fixed email layout

--- resources/views/vendor/mail/html/layout.blade.php
+++ resources/views/vendor/mail/html/layout.blade.php
@@ -35,7 +35,7 @@
             <td align="center">
                 <table class="content" width="100%" cellpadding="0" cellspacing="0" role="presentation">
                     {!! $header ?? '' !!}
-
+                    
                     <!-- Email Body -->
                     <tr>
                         <td class="body" width="100%" cellpadding="0" cellspacing="0"
